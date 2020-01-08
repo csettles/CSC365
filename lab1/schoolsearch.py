37dcd501c24e2ddbf812d9cea1f21a14bae27725
Ty Farris <tefarris@calpoly.edu>
2020-01-08 11:28:36 -0800

    Parsing input

--- lab1/schoolsearch.py
+++ lab1/schoolsearch.py
@@ -19,3 +19,40 @@ def parse_file(file_name):
 
 if __name__ == '__main__':
     parse_file('students.txt')
+    command = input("Enter in a command: ")
+    command = command.split(" ")
+
+    while command[0] != "Quit" and command[0] != "Q":
+        if len(command) <= 3 and (command[0] == "Student" or command[0] == "S"):
+            if command[2] == "B" or command[2] == "Bus":
+                print("Bus")
+
+        elif len(command) <= 2 and (command[0] == "Teacher" or command[0] == "T"):
+            print("Teacher")
+
+        elif len(command) <= 2 and (command[0] == "Bus" or command[0] == "B"):
+            print("Bus")
+
+        elif len(command) <= 3 and (command[0] == "Grade" or command[0] == "G"):
+            if command[2] == "High" or command[2] == "H":
+                print("High")
+            if command[2] == "Low" or command[2] == "L":
+                print("Low") 
+
+        elif len(command) <= 2 and (command[0] == "Average" or command[0] == "A"):
+            print("Average")
+
+        elif len(command) == 1 and (command[0] == "Info" or command[0] == "I"):
+            print("Info")
+
+        elif len(command) == 1 and (command[0] == "Quit" or command[0] == "Q"):
+            print("Quit")
+
+        else:
+            print("Invalid input. ")
+        
+        command = input("Enter in a command: ")
+        command = command.split(" ")
+
+    if len(command) > 1 and (command[0] != "Quit" or command[0] != "Q"):
+        print("Invalid command. Restart program.")
